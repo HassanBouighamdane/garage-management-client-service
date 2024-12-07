b96a099d88f7a96ed16eabc6a133a06ec6256446
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2024-12-07 16:20:19 +0100

    first commit

---
+++ src/main/java/com/exemple/garagemanagementclientservice/GarageManagementClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.garagemanagementclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageManagementClientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageManagementClientServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exemple/garagemanagementclientservice/GarageManagementClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.garagemanagementclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageManagementClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
